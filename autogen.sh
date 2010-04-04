dc06c984f6a3b6e2930b67e078940e29d928e9a9
Brian Harring <ferringb@gmail.com>
2010-04-04 13:37:06 -0400

    fix the autogen... again.
    SVN-Revision: 2075

--- autogen.sh
+++ autogen.sh
@@ -2,8 +2,8 @@
 
 aclocal-1.10 || { echo "failed aclocal"; exit 1; };
 libtoolize --automake -c -f || { echo "failed libtoolize"; exit 1; }
+autoreconf --install || { echo "failed autoreconf"; exit 1; }
 automake --add-missing || { echo "automake fialed"; exit 1; }
-autoreconf || { echo "failed autoreconf"; exit 1; }
 
 if [ -x ./test.sh ] ; then
 	exec ./test.sh "$@"
