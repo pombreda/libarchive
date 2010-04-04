7f3786cf147ac28a66aa99d4774c369c2b4b29a6
Brian Harring <ferringb@gmail.com>
2010-04-04 14:29:36 -0400

    initial hacking of it; assumptions re: archive_entry usage are fundamentally wrong, thus about to rework it (archive_entry's must be cloned if read_next_header is triggered)
    SVN-Revision: 2162

---
+++ src/pyarchive.c
@@ -0,0 +1,245 @@
+// Copyright: 2009 Brian Harring <ferringb@gmail.com>
+// License: GPL2/BSD
+
+#include <Python.h>
+#include <archive.h>
+#include <archive_entry.h>
+
+#define DEFAULT_BUFSIZE 4096
+
+typedef struct {
+    PyObject_HEAD
+    struct archive *archive;
+    PyObject *last_PyArchiveEntry;
+} PyArchive;
+
+typedef struct {
+    PyObject_HEAD
+    PyArchive *archive;
+    struct archive_entry *archive_entry;
+} PyArchiveEntry;
+
+
+static PyArchiveEntry *_mk_archive_entry(PyArchive *,
+    struct archive_entry *);
+
+#define MAX_FREE_ARCHIVE_ENTRY 0
+static PyArchiveEntry *archive_entry_freelist[MAX_FREE_ARCHIVE_ENTRY];
+static int archive_entry_free_count = 0;
+
+static void
+PyArchiveEntry_Dealloc(PyArchiveEntry *pae)
+{
+    PyObject *archive = (PyObject *)pae->archive;
+    PyObject_GC_UnTrack(pae);
+    if(archive_entry_free_count < MAX_FREE_ARCHIVE_ENTRY) {
+        archive_entry_clear(pae->archive_entry);
+        archive_entry_freelist[archive_entry_free_count++] = pae;
+    } else {
+        //archive_entry_free(pae->archive_entry);
+        pae->ob_type->tp_free((PyObject *)pae);
+    }
+    Py_XDECREF(archive);
+}
+
+
+static PyTypeObject PyArchiveEntryType = {
+    PyObject_HEAD_INIT(NULL)
+    0,                                /* ob_size */
+    "archive_entry",                  /* tp_name */
+    sizeof(PyArchiveEntry),   /* tp_basicsize */
+    0,                                /* tp_itemsize */
+    (destructor)PyArchiveEntry_Dealloc,
+                                      /* tp_dealloc */
+    0,                                /* tp_print */
+    0,                                /* tp_getattr */
+    0,                                /* tp_setattr */
+    0,                                /* tp_compare */
+    0,                                /* tp_repr */
+    0,                                /* tp_as_number */
+    0,                                /* tp_as_sequence */
+    0,                                /* tp_as_mapping */
+    0,                                /* tp_hash */
+    0,                                /* tp_call */
+    0,                                /* tp_str */
+    0,                                /* tp_getattro */
+    0,                                /* tp_setattro */
+    0,                                /* tp_as_buffer */
+    Py_TPFLAGS_DEFAULT | Py_TPFLAGS_BASETYPE, /* tp_flags */
+    0,                                /* tp_doc */
+    0,                                /* tp_traverse */
+    0,                                /* tp_clear */
+    0,                                /* tp_richcompare */
+    0,                                /* tp_weaklistoffset */
+    0,                                /* tp_iter */
+    0,                                /* tp_iternext */
+    0,                                /* tp_methods */
+    0,                                /* tp_members */
+    0,                                /* tp_getset */
+    0,                                /* tp_base */
+    0,                                /* tp_dict */
+    0,                                /* tp_descr_get */
+    0,                                /* tp_descr_set */
+    0,                                /* tp_dictoffset */
+    0,                                /* tp_init */
+    PyType_GenericAlloc,              /* tp_alloc */
+    PyType_GenericNew,                /* tp_new */
+    PyObject_GC_Del,                  /* tp_free */
+};
+    
+
+static PyArchiveEntry *
+mk_PyArchiveEntry(PyArchive *archive)
+{
+    PyArchiveEntry *pae;
+    if(archive_entry_free_count) {
+        archive_entry_free_count--;
+        pae = archive_entry_freelist[archive_entry_free_count];
+        _Py_NewReference((PyObject *)pae);
+    } else {
+        pae = PyObject_GC_New(PyArchiveEntry,
+            &PyArchiveEntryType);
+        if(!pae)
+            return NULL;
+        struct archive_entry *ae = NULL;//archive_entry_new();
+        if(!ae) {
+            Py_DECREF(pae);
+            return NULL;
+        }
+        pae->archive_entry = ae;
+    }
+    Py_INCREF(archive);
+    pae->archive = archive;
+    return pae;
+}
+
+
+PyObject *
+PyArchive_new(PyTypeObject *type, PyObject *args, PyObject *kwds)
+{
+    PyArchive *self = (PyArchive *)type->tp_alloc(type, 0);
+    if(self)
+        self->archive = NULL;
+    return (PyObject *)self;
+}
+
+
+PyObject *
+PyArchive_init(PyArchive *self, PyObject *args, PyObject *kwds)
+{
+    PyObject *filepath = NULL;
+    if(!PyArg_ParseTuple(args, "S", &filepath))
+        return NULL;
+    if(NULL == (self->archive = archive_read_new()))
+        return NULL;
+    // doesn't set an exception.
+    // error checking in general.
+    if(ARCHIVE_OK != archive_read_support_compression_all(self->archive))
+        return NULL;
+    if(ARCHIVE_OK != archive_read_support_format_all(self->archive))
+        return NULL;
+    int ret;
+    if(ARCHIVE_OK != (ret = archive_read_open_file(self->archive,
+        PyString_AsString(filepath), DEFAULT_BUFSIZE))) {
+        printf("bailing w/ %i\n", ret);
+        printf("%s\n", archive_error_string(self->archive));
+        archive_read_finish(self->archive);
+        self->archive = NULL;
+        return NULL;
+    }
+    return NULL;
+}
+
+
+static void
+PyArchive_dealloc(PyArchive *self)
+{
+    printf("deallocating a pyarchive\n");
+    if(self->archive) {
+        archive_read_finish(self->archive);
+        self->archive = NULL;
+    }
+    Py_CLEAR(self->last_PyArchiveEntry);
+    self->ob_type->tp_free((PyObject *)self);
+}
+
+
+static PyObject *
+PyArchive_iternext(PyArchive *self)
+{
+    PyArchiveEntry *tmp = NULL, *pae = mk_PyArchiveEntry(self);
+    if(pae) {
+        int ret = archive_read_next_header(self->archive, &pae->archive_entry);
+        if(ret != ARCHIVE_OK) {
+            Py_DECREF(pae);
+        }
+        tmp = (PyArchiveEntry *)self->last_PyArchiveEntry;
+        self->last_PyArchiveEntry = (PyObject *)pae;
+        Py_XDECREF(tmp);
+        Py_INCREF(pae);
+    }
+    return (PyObject *)pae;
+}
+
+static PyTypeObject PyArchiveType = {
+    PyObject_HEAD_INIT(NULL)
+    0,                                /* ob_size */
+    "archive",                        /* tp_name */
+    sizeof(PyArchive),       /* tp_basicsize */
+    0,                                /* tp_itemsize */
+    (destructor)PyArchive_dealloc,     /* tp_dealloc */
+    0,                                /* tp_print */
+    0,                                /* tp_getattr */
+    0,                                /* tp_setattr */
+    0,                                /* tp_compare */
+    0,                                /* tp_repr */
+    0,                                /* tp_as_number */
+    0,                                /* tp_as_sequence */
+    0,                                /* tp_as_mapping */
+    0,                                /* tp_hash */
+    0,                                /* tp_call */
+    0,                                /* tp_str */
+    0,                                /* tp_getattro */
+    0,                                /* tp_setattro */
+    0,                                /* tp_as_buffer */
+    Py_TPFLAGS_DEFAULT | Py_TPFLAGS_BASETYPE, /* tp_flags */
+    0,                                /* tp_doc */
+    0,                                /* tp_traverse */
+    0,                                /* tp_clear */
+    0,                                /* tp_richcompare */
+    0,                                /* tp_weaklistoffset */
+    (getiterfunc)PyObject_SelfIter,   /* tp_iter */
+    (iternextfunc)PyArchive_iternext, /* tp_iternext */
+    0,                                /* tp_methods */
+    0,                                /* tp_members */
+    0,                                /* tp_getset */
+    0,                                /* tp_base */
+    0,                                /* tp_dict */
+    0,                                /* tp_descr_get */
+    0,                                /* tp_descr_set */
+    0,                                /* tp_dictoffset */
+    (initproc)PyArchive_init,          /* tp_init */
+    0,                                /* tp_alloc */
+    PyArchive_new,                     /* tp_new */
+};
+
+PyDoc_STRVAR(
+    Pydocumentation,
+    "fill this out");
+
+PyMODINIT_FUNC
+initpyarchive()
+{
+    PyObject *m = Py_InitModule3("pyarchive", NULL,
+        Pydocumentation);
+    if(!m)
+        return;
+
+    if(PyType_Ready(&PyArchiveType) < 0)
+        return;
+
+    if(PyModule_AddObject(m, "archive",
+        (PyObject *)&PyArchiveType) == -1)
+        return;
+}
+
