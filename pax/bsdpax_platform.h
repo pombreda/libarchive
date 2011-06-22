a5cc46ee3c102cd7d738a586e97f769fb7022d91
Michihiro NAKAJIMA <ggcueroad@gmail.com>
2011-06-22 08:25:56 -0400

    Remove unused macros and stop including unneeded files.
    SVN-Revision: 3416

--- pax/bsdpax_platform.h
+++ pax/bsdpax_platform.h
@@ -62,59 +62,6 @@
 #include "archive_entry.h"
 #endif
 
-#ifdef HAVE_LIBACL
-#include <acl/libacl.h>
-#endif
-
-/*
- * Include "dirent.h" (or it's equivalent on several different platforms).
- *
- * This is slightly modified from the GNU autoconf recipe.
- * In particular, FreeBSD includes d_namlen in it's dirent structure,
- * so my configure script includes an explicit test for the d_namlen
- * field.
- */
-#if HAVE_DIRENT_H
-# include <dirent.h>
-# if HAVE_DIRENT_D_NAMLEN
-#  define DIRENT_NAMLEN(dirent) (dirent)->d_namlen
-# else
-#  define DIRENT_NAMLEN(dirent) strlen((dirent)->d_name)
-# endif
-#else
-# define dirent direct
-# define DIRENT_NAMLEN(dirent) (dirent)->d_namlen
-# if HAVE_SYS_NDIR_H
-#  include <sys/ndir.h>
-# endif
-# if HAVE_SYS_DIR_H
-#  include <sys/dir.h>
-# endif
-# if HAVE_NDIR_H
-#  include <ndir.h>
-# endif
-#endif
-
-#if HAVE_STRUCT_STAT_ST_MTIMESPEC_TV_NSEC
-#define	ARCHIVE_STAT_CTIME_NANOS(st)	(st)->st_ctimespec.tv_nsec
-#define	ARCHIVE_STAT_MTIME_NANOS(st)	(st)->st_mtimespec.tv_nsec
-#elif HAVE_STRUCT_STAT_ST_MTIM_TV_NSEC
-#define	ARCHIVE_STAT_CTIME_NANOS(st)	(st)->st_ctim.tv_nsec
-#define	ARCHIVE_STAT_MTIME_NANOS(st)	(st)->st_mtim.tv_nsec
-#elif HAVE_STRUCT_STAT_ST_MTIME_N
-#define	ARCHIVE_STAT_CTIME_NANOS(st)	(st)->st_ctime_n
-#define	ARCHIVE_STAT_MTIME_NANOS(st)	(st)->st_mtime_n
-#elif HAVE_STRUCT_STAT_ST_UMTIME
-#define	ARCHIVE_STAT_CTIME_NANOS(st)	(st)->st_uctime * 1000
-#define	ARCHIVE_STAT_MTIME_NANOS(st)	(st)->st_umtime * 1000
-#elif HAVE_STRUCT_STAT_ST_MTIME_USEC
-#define	ARCHIVE_STAT_CTIME_NANOS(st)	(st)->st_ctime_usec * 1000
-#define	ARCHIVE_STAT_MTIME_NANOS(st)	(st)->st_mtime_usec * 1000
-#else
-#define	ARCHIVE_STAT_CTIME_NANOS(st)	(0)
-#define	ARCHIVE_STAT_MTIME_NANOS(st)	(0)
-#endif
-
 /* How to mark functions that don't return. */
 /* This facilitates use of some newer static code analysis tools. */
 #undef __LA_DEAD
